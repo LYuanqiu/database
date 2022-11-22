8e266e8074496a215ada41f215c0575a6c9919da
lvyuanqiu <1246847701@qq.com>
2022-11-22 12:39:54 +0800

    商品页+个人信息页初步

---
+++ src/components/customer/customer_Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home">
+    <div class="header">头部</div>
+    <template v-if="show">
+      <div class="header">
+        <router-link to="/customerHome"
+                     class="header-title">电商网站首页</router-link>
+        <div class="header-menu">
+          {{user}}
+        </div>
+        <div class="header-menu">
+          <router-link to="/cart"
+                       class="header-menu-cart">
+            购物车
+            <span v-if="cartList.length">{{cartList.length}}</span>
+          </router-link>
+        </div>
+
+        <div class="header-menu">
+          <router-link to="/login/logout"
+                       class="header-menu-cart">
+            退出登录
+          </router-link>
+        </div>
+        <customerList></customerList>
+      </div>
+      <router-view></router-view>
+    </template>
+    <!-- 挂载所有路由 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import customerList from "./customer_list.vue"
+export default {
+  components: {
+    customerList
+  },
+  data(){
+    return {
+      user: this.$store.state.username
+    }
+  },
+  computed: {
+    cartList(){
+      return this.$store.state.cartList;
+    },
+    show(){
+      return true;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  position: absolute;
+  line-height: 40px;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+}
+body{
+  margin:0;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,78 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Vuex from 'vuex'
+import ElementUI from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Router)
+Vue.use(Vuex);
+Vue.use(ElementUI)
+
+/*
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+    routes // (缩写) 相当于 routes: routes
+})
+*/
+
+export default new Router({
+    routes: [
+        {
+            path: '/customerHome',
+            meta: {
+                title: '首页'
+            },
+            component: (resolve) => require(['../components/customer/customer_Home'], resolve)
+        },
+        {
+            path: '/list',
+            meta: {
+                title: '首页'
+            },
+            component: (resolve) => require(['../components/customer/customer_list'], resolve)
+        },
+        {
+            path: '/product/:id',
+            meta: {
+                title: '商品详情'
+            },
+            component: (resolve) => require(['../components/customer/customer_product_view.vue'], resolve)
+        },
+        {
+            path: '/cart',
+            meta: {
+                title: '购物车'
+            },
+            component: (resolve) => require(['../components/customer/customer_cart.vue'], resolve)
+        },
+        {
+            path: '/login/:loginStatus',
+            meta: {
+                title: '登录注册'
+            },
+            component: (resolve) => require(['../components/customer/login.vue'], resolve)
+        },
+        {
+            path: '/customerPerson',
+            meta: {
+                title: '个人中心'
+            },
+            component: (resolve) => require(['../components/customer/customer_personal'], resolve)
+        },
+        {
+            path: '*',
+            redirect: '/login/login',
+            //component: (resolve) => require(['../components/customer/login'], resolve)
+            component: (resolve) => require(['../components/customer/customer_Home.vue'], resolve)
+        }
+    ],
+    mode: "history" // 去除访问时候带#问题
+})
+
